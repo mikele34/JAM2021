e48dfd772fb0fe13fa9a4306697727da2ff0317a
mikele34 <82645362+mikele34@users.noreply.github.com>
2021-05-27 16:29:06 +0200

    CameraManager base

---
+++ JAM2021/Assets/Scripts/CameraManager.cs
@@ -0,0 +1,13 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraManager : MonoBehaviour
+{
+    public Transform Player;
+
+    void Update()
+    {
+        transform.position = new Vector3(Player.position.x, transform.position.y, Player.position.z);
+    }
+}
